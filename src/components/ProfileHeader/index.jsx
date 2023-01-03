1afb6c8557d9cf536d3bda0bb8d2742c2bcc3e39
yahya sh <yahya240_1@hotmail.com>
2023-01-03 15:12:30 +0200

    adding useParams for the profile path

--- src/components/ProfileHeader/index.jsx
+++ src/components/ProfileHeader/index.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react'
+import { useParams } from 'react-router'
 import profileImage from "../../assets/images/gamer-profile/profile-image.png"
 import './style.css'
 
-export default class ProfileHeader extends Component {
-
-  render() {
-    const {email} = this.props.user;
-    return (
-        <div className='gamer-profile-header'>
-        <div className="gamer-profile-header-container">
-          <div className="gamer-profile-header-content">
-            <h2>Welcome back,</h2>
-            <h2>{email.split('@')[0]}</h2>
-          </div>
-          <img src={profileImage} alt="profileImage" />
+const ProfileHeader = () => {
+  const {id} = useParams();
+  return (
+      <div className='gamer-profile-header'>
+      <div className="gamer-profile-header-container">
+        <div className="gamer-profile-header-content">
+          <h2>Welcome back,</h2>
+          <h2>{id}</h2>
         </div>
+        <img src={profileImage} alt="profileImage" />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ProfileHeader;
